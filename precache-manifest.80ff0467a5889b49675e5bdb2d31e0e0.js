b6bf2bc244db1348f2b9c4bd8221e92a0ee96dbc
unknown <behzadshahid95@gmail.com>
2020-10-11 14:39:38 +0500

    Updates

---
+++ precache-manifest.80ff0467a5889b49675e5bdb2d31e0e0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5aeb4600b435c8adb9bd48e9102c399",
+    "url": "/BehzadShahid319.github.io/Web-Assignemnt01/index.html"
+  },
+  {
+    "revision": "de6c9a0a94187e8b979b",
+    "url": "/BehzadShahid319.github.io/Web-Assignemnt01/static/css/main.71b81f8c.chunk.css"
+  },
+  {
+    "revision": "abaec4489316804ac41f",
+    "url": "/BehzadShahid319.github.io/Web-Assignemnt01/static/js/2.6fa80c51.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/BehzadShahid319.github.io/Web-Assignemnt01/static/js/2.6fa80c51.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de6c9a0a94187e8b979b",
+    "url": "/BehzadShahid319.github.io/Web-Assignemnt01/static/js/main.adf9d9d4.chunk.js"
+  },
+  {
+    "revision": "f89417bce33a98876ce5",
+    "url": "/BehzadShahid319.github.io/Web-Assignemnt01/static/js/runtime-main.8b0a1a80.js"
+  },
+  {
+    "revision": "dae6d43d8e645e5ea1f27a2cdf27dd34",
+    "url": "/BehzadShahid319.github.io/Web-Assignemnt01/static/media/WreckCover.dae6d43d.png"
+  },
+  {
+    "revision": "17c7d7b83a98e3cc813c5e17b6e3aa02",
+    "url": "/BehzadShahid319.github.io/Web-Assignemnt01/static/media/WreckExplo.17c7d7b8.mp4"
+  }
+]);
